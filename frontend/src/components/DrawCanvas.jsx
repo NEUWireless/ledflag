299265d42f276dbc62f00b588627024162c23e02
packard-j <packard-j@husky.neu.edu>
2019-12-07 23:24:25 -0500

    Added color picker and radius slider

--- frontend/src/components/DrawCanvas.jsx
+++ frontend/src/components/DrawCanvas.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
-import { Button } from "antd";
+import { Button, Slider } from "antd";
+import { TwitterPicker } from "react-color";
 import "antd/dist/antd.css";
 import "../css/DrawCanvas.css"
 
@@ -38,17 +39,15 @@ function DrawCanvas(props) {
   const [isDrawing, setIsDrawing] = useState(false);
   const [drawRadius, setDrawRadius] = useState(2);
   const [drawColor, setDrawColor] = useState([65, 230, 124]);
-
   const canvasRef = useRef();
-  const leds = [];
-  for (let i = 0; i < LEDS_X * LEDS_Y; i++) {
-    leds[i] = [0, 0, 0];
-  }
+
+  const [leds, setLeds] = useState([]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const _ctx = canvas.getContext('2d');
     drawMatrix(_ctx);
+    fillLeds([0, 0, 0]);
     setCtx(_ctx);
   }, []);
 
@@ -63,23 +62,36 @@ function DrawCanvas(props) {
     draw(offsetX, offsetY);
   };
 
+  const onTouchMove = event => {
+    const rect = event.target.getBoundingClientRect();
+    const x = event.targetTouches[0].pageX - rect.left;
+    const y = event.targetTouches[0].pageY - rect.top;
+    draw(x, y);
+  };
+
+  const startDrawing = () => setIsDrawing(true);
+
+  const stopDrawing = () => setIsDrawing(false);
+
   const draw = (offsetX, offsetY) => {
     const [ix, iy] = [posToIdx(offsetX), posToIdx(offsetY)];
     const diff = drawRadius;
     ctx.fillStyle = `rgb(${drawColor[0]}, ${drawColor[1]}, ${drawColor[2]})`;
     const modified = [];
-    for (let y = iy - diff; y < iy + diff; y++) {
-      for (let x = ix - diff; x < ix + diff; x++) {
+    for (let y = Math.max(0, iy - diff); y < Math.min(iy + diff, LEDS_Y); y++) {
+      for (let x = Math.max(0, ix - diff); x < Math.min(ix + diff, LEDS_X); x++) {
         const ledIndex = y * LEDS_Y + x;
         if (Math.sqrt(Math.pow(offsetX - idxToPos(x)  , 2) +
           Math.pow(offsetY - idxToPos(y), 2)) <= drawRadius * 2 * RADIUS &&
           !colorsEqual(drawColor, leds[ledIndex])) {
-          leds[ledIndex] = drawColor;
           drawLED(ctx, x, y);
           modified.push({x, y, r: drawColor[0], g: drawColor[1], b: drawColor[2]});
         }
       }
     }
+    const newLeds = [...leds];
+    modified.forEach(p => newLeds[p.y * LEDS_Y + p.x] = [p.r, p.g, p.b]);
+    setLeds(newLeds);
     if (modified.length > 0) {
       console.log(modified);
       props.socket.emit('draw', {pixels: modified});
@@ -90,20 +102,32 @@ function DrawCanvas(props) {
     fetch("/clear", {
       method: "GET"
     }).then(res => console.log(res));
-    for (let i = 0; i < LEDS_X * LEDS_Y; i++) {
-      leds[i] = [0, 0, 0];
-    }
+    fillLeds([0, 0, 0]);
     drawMatrix(ctx);
+  };
+
+  const fillLeds = clr => {
+    const newLeds = [];
+    for (let i = 0; i < LEDS_X * LEDS_Y; i++) {
+      newLeds[i] = clr;
+    }
+    setLeds(newLeds);
   };
 
   return (
     <div className="draw">
       <canvas className="canvas" ref={canvasRef} width={WIDTH} height={HEIGHT}
-              onMouseDown={() => setIsDrawing(true)}
-              onMouseUp={() => setIsDrawing(false)}
+              onMouseDown={startDrawing}
+              onMouseUp={stopDrawing}
               onMouseMove={onMouseMove}
+              onTouchStart={startDrawing}
+              onTouchEnd={stopDrawing}
+              onTouchMove={onTouchMove}
               onClick={onMouseClick}
       />
+      <TwitterPicker color={{r: drawColor[0], g: drawColor[1], b: drawColor[2]}}
+                     onChange={clr => setDrawColor([clr.rgb.r, clr.rgb.g, clr.rgb.b])}/>
+      <Slider value={drawRadius} onChange={setDrawRadius} min={1} max={10}/>
       <Button onClick={clear}>Clear</Button>
     </div>
   )
