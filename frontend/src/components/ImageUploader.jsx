343021474c0379c7b80972bd78fee2082165b8ce
packard-j <packard-j@husky.neu.edu>
2020-01-18 14:18:57 -0500

    Added image fit

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -1,21 +1,69 @@
-import React from "react";
+import React, {useRef, useState} from "react";
+import { Form, Select } from 'antd';
+
+const { Option } = Select;
 
 function ImageUploader(props) {
 
-  const imageHandler = event => {
-    console.log("ImageUploader Handler invoked");
-    const width = 64;
-    const height = 32;
-    const ctx = document.createElement("canvas").getContext('2d');
-    const url = URL.createObjectURL(event.target.files[0]);
+  const WIDTH = 64;
+  const HEIGHT = 32;
+
+  const canvasRef = useRef();
+  const [mode, setMode] = useState("stretch");
+  const [sourceImage, setSourceImage] = useState(null);
+
+  const onImageChange = event => {
     const img = new Image();
-    img.onload = function() {
-      ctx.drawImage(img, 0, 0, width, height);
-      console.log("Draw complete");
-      const data = ctx.getImageData(0, 0, width, height).data;
-      callApi(data).then(success => console.log(success));
-    };
-    img.src = url;
+    img.onload = () => drawAndSend(img, canvasRef.current.getContext("2d"), mode);
+    img.src = URL.createObjectURL(event.target.files[0]);
+    setSourceImage(img);
+  };
+
+  const onModeChange = value => {
+    setMode(value);
+    if (sourceImage) {
+      drawAndSend(sourceImage, canvasRef.current.getContext('2d'), value);
+    }
+  };
+
+  const drawAndSend = (img, ctx, mode) => {
+    draw(img, ctx, mode);
+    const data = ctx.getImageData(0, 0, WIDTH, HEIGHT).data;
+    callApi(data).then(success => console.log(success));
+  };
+
+  const draw = (img, ctx, mode) => {
+    switch (mode) {
+      case "fit":
+        drawFit(img, ctx);
+        break;
+      default:
+        drawStretch(img, ctx);
+    }
+  };
+
+  const drawFit = (img, ctx) => {
+
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    const screenAspect = WIDTH / HEIGHT;
+    const imgAspect = img.width / img.height;
+
+    const scale = screenAspect > imgAspect ?
+      HEIGHT / img.height : WIDTH / img.width;
+
+    const dWidth = img.width * scale;
+    const dHeight = img.height * scale;
+
+    const dx = (WIDTH - dWidth) / 2;
+    const dy = (HEIGHT - dHeight) / 2;
+
+    ctx.drawImage(img, 0, 0, img.width, img.height, dx, dy, dWidth, dHeight);
+  };
+
+  const drawStretch = (img, ctx) => {
+    ctx.drawImage(img, 0, 0, WIDTH, HEIGHT);
   };
 
   const callApi = async imageData => {
@@ -33,7 +81,18 @@ function ImageUploader(props) {
   };
 
   return <div>
-    <input type="file" accept="image/*" onChange={imageHandler}/>
+    <Form>
+      <Form.Item>
+        <input type="file" accept="image/*" onChange={onImageChange}/>
+        <Select value={mode} onChange={onModeChange}>
+          <Option value="stretch">Stretch</Option>
+          <Option value="fit">Fit</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <canvas width={WIDTH} height={HEIGHT} ref={canvasRef}/>
+      </Form.Item>
+    </Form>
   </div>
 
 }
